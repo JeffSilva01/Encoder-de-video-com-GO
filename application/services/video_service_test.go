c25888f6f3ba0897aa4fc12647bc86afe6b9813d
Jefferson Silva <jeffsilva01.dev@gmail.com>
2024-09-22 13:15:31 -0300

    create video service

---
+++ application/services/video_service_test.go
@@ -0,0 +1,48 @@
+package services_test
+
+import (
+	"encoder/application/repositories"
+	"encoder/application/services"
+	"encoder/domain"
+	"encoder/famework/database"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "video.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	return video, repo
+}
+
+func TestVideoService_Download(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("fullcycle-go")
+	require.Nil(t, err)
+}
--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module encoder
 go 1.14
 
 require (
+	cloud.google.com/go/storage v1.43.0
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.9.3
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.9.0
 )
